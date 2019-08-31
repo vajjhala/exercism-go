1b375c278a45cc85c32886735e57be376b53608c
Vajjhala <v2.surya@gmail.com>
2019-08-31 18:44:49 -0400

    Hamming complete

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -5,19 +5,18 @@ import "errors"
 
 func hammingDist(a, b string) int {
 	count := 0
-
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			count = count + 1
+		}
+	}
 	return count
 }
 
 //Distance calculates the hamming distance between two DNA strangs and returns an integer and the errors.
 func Distance(a, b string) (int, error) {
-	if len(a) != len(b) {
-		return 0, errors.New("The lengths of the two DNA strands must be equal")
+	if len(a) == len(b) {
+		return hammingDist(a, b), nil
 	}
-	if a == b {
-		return 0, nil
-	}
-
-	count := hammingDist(a, b)
-	return count, nil
+	return 0, errors.New("Error")
 }
