19e1cad86545531ad3687b1244447261f9609939
Vajjhala <v2.surya@gmail.com>
2019-09-03 23:54:20 -0400

    started work on triangle

--- triangle/triangle.go
+++ triangle/triangle.go
@@ -1,13 +1,8 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package triangle should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package triangle
 package triangle
 
-
-// Notice KindFromSides() returns this type. Pick a suitable data type.
 type Kind
+
 
 const (
     // Pick values for the following identifiers used by the test program.
@@ -17,12 +12,8 @@ const (
     Sca // scalene
 )
 
-// KindFromSides should have a comment documenting it.
+// KindFromSides takes the lengths of the three sides of a triangle and returns the Kind of triangle
 func KindFromSides(a, b, c float64) Kind {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
 	var k Kind
 	return k
 }
