ec5c33cf32cab8bcc3ac321a0e85b4a10473ce62
Vajjhala <v2.surya@gmail.com>
2019-10-03 19:38:33 -0400

    etl started

---
+++ etl/etl.go
@@ -0,0 +1,19 @@
+//Package etl to transform scores
+package etl
+
+import ("strings")
+
+out := make(map[string]int)
+
+func load(vals []string) {
+    for _,v := range vals {
+        
+    }
+}
+
+//Transform (map[int][]string) map[string]int
+func Transform(in map[int][]string) map[string]int {
+	for k, v := range in {
+
+	}
+}
