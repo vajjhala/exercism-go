9527d8a155ae91fdc5af9705caff6053c4f3a8ec
Vajjhala <v2.surya@gmail.com>
2019-10-03 21:23:22 -0400

    etl PASS

--- etl/etl.go
+++ etl/etl.go
@@ -1,19 +1,17 @@
 //Package etl to transform scores
 package etl
 
-import ("strings")
-
-out := make(map[string]int)
-
-func load(vals []string) {
-    for _,v := range vals {
-        
-    }
-}
+import (
+	"strings"
+)
 
 //Transform (map[int][]string) map[string]int
 func Transform(in map[int][]string) map[string]int {
-	for k, v := range in {
-
+	out := make(map[string]int)
+	for k, arr := range in {
+		for _, v := range arr {
+			out[strings.ToLower(v)] = k
+		}
 	}
+	return out
 }
