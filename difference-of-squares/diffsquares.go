e9c1cf8286a700f3515693e721621086f6ed9cda
vajjhala <vajjhala@bu.edu>
2019-10-19 23:44:25 -0400

    added golint comments

--- difference-of-squares/diffsquares.go
+++ difference-of-squares/diffsquares.go
@@ -1,14 +1,18 @@
+//Package diffsquares to calculate the difference between the sum of squares and
+//square of sums
 package diffsquares
 
-
+//SquareOfSum calculates (1 + 2 + 3 + ... N)^2
 func SquareOfSum(n int) int {
-  return n*n*(n+1)*(n+1)/4
+	return n * n * (n + 1) * (n + 1) / 4
 }
 
+//SumOfSquares calculates 1^2 + 2^2 + 3^2 + ... N^2
 func SumOfSquares(n int) int {
-  return n* (2*n + 1) * (n + 1) / 6
+	return n * (2*n + 1) * (n + 1) / 6
 }
 
+//Difference calculates (1 + 2 + 3 + ... N)^2  -   (1^2 + 2^2 + 3^2 + ... N^2)
 func Difference(n int) int {
-    return SquareOfSum(n) - SumOfSquares(n)
+	return SquareOfSum(n) - SumOfSquares(n)
 }
