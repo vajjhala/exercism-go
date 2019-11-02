cbc98e6f9041ec194f35da801933204948d4d978
vajjhala <vajjhala@bu.edu>
2019-11-02 17:37:27 -0400

    New exercis: Luhn

--- sum-of-multiples/sum_of_multiples.go
+++ sum-of-multiples/sum_of_multiples.go
@@ -1,1 +1,25 @@
 package summultiples
+
+import "fmt"
+
+//SumMultiples (lmt, divisors) returns int
+func SumMultiples(lmt int, divisors ...int) int {
+	sum := 0
+	for i := 0; i < len(divisors); i++ {
+		if divisors[i] <= 0 {
+			continue
+		}
+		j := 0
+		var val int
+		val = divisors[i] * j
+		for val < lmt {
+			fmt.Printf("divisor * j < lmt |  %d (%d * %d) < %d\n",
+				val, divisors[i], j, lmt)
+			sum = sum + val
+			val = divisors[i] * j
+			j++
+		}
+		fmt.Println()
+	}
+	return sum
+}
