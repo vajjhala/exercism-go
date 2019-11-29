75a6608d7a927c34622d044e3c8a3006f706bc29
vajjhala <vajjhala@bu.edu>
2019-11-29 14:17:38 -0500

    working condition luhn

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -2,32 +2,41 @@
 package luhn
 
 import (
+	//"fmt"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 //Valid returns if a string is a valid Luhn string
 func Valid(s string) bool {
 	//step 0: check if valid Luhn string
-	mtch, _ := regexp.MatchString(`^\d(\d|" ")*$`, s)
+	pattern := `^\d{1}(\d|\s)+$`
+	mtch, _ := regexp.MatchString(pattern, s)
 	if !mtch {
 		return false
 	}
-	re := regexp.MustCompile(`^\d(\d|" ")*$`)
-	s = re.ReplaceAllString(" ", "")
+	//re := regexp.MustCompile(pattern)
+	s = strings.Replace(s, " ", "", -1)
 	//step 1: compute sum
 	runes := []rune(s)
-	slen, sum, digit := len(runes), 0, 0
-	for i := 1; i < slen; i++ {
-		digit, ok := strconv.Atoi(strconv.QuoteRuneToASCII(runes[slen-i]))
-		if ok != nil {
+	slen, sum := len(runes), 0
+	//fmt.Printf("\nrunes: %s, len: %d\n", s, slen)
+	for i := 1; i <= slen; i++ {
+		digit, err := strconv.ParseInt(string(runes[slen-i]), 10, 8)
+		if err != nil {
 			return false
 		}
 		if i%2 == 0 {
-			digit = digit % 9
+			digit = (digit * 2)
 		}
-		sum = sum + digit
+		if digit > 9 {
+			digit = digit - 9
+		}
+
+		sum = sum + int(digit)
 	}
-	//step 2: check sum is evenly divisible by 10
-	return (digit%10 == 0)
+
+	//step 2: return remainder of sum
+	return (sum%10 == 0)
 }
