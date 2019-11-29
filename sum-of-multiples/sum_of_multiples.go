ae64ae8467d8966f55cf1e5b7ec3ce932b0c454c
vajjhala <vajjhala@bu.edu>
2019-11-29 18:44:17 -0500

    completed sum-of-multiples

--- sum-of-multiples/sum_of_multiples.go
+++ sum-of-multiples/sum_of_multiples.go
@@ -1,25 +1,27 @@
 package summultiples
 
-import "fmt"
-
 //SumMultiples (lmt, divisors) returns int
 func SumMultiples(lmt int, divisors ...int) int {
+	//fmt.Printf("\nInput: %v, limit: %d\n", divisors, lmt)
 	sum := 0
+	var uniq = make(map[int]bool, lmt)
 	for i := 0; i < len(divisors); i++ {
-		if divisors[i] <= 0 {
+		num := divisors[i]
+		//fmt.Printf("divisor: %d\n", num)
+		if num <= 0 {
 			continue
 		}
-		j := 0
-		var val int
-		val = divisors[i] * j
-		for val < lmt {
-			fmt.Printf("divisor * j < lmt |  %d (%d * %d) < %d\n",
-				val, divisors[i], j, lmt)
-			sum = sum + val
-			val = divisors[i] * j
-			j++
+		//fmt.Print("\tmultiples: ")
+		for j := 1; num*j < lmt; j++ {
+			v := num * j
+			if uniq[v] {
+				continue
+			}
+			sum = sum + v
+			uniq[v] = true
+			//fmt.Printf("%d (%d), ", num*j, sum)
 		}
-		fmt.Println()
+		//fmt.Println()
 	}
 	return sum
 }
