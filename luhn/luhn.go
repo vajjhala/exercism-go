532aa4e8ff71a1c50212e3432f8d26600283ca8b
vajjhala <vajjhala@bu.edu>
2019-12-07 12:44:50 -0500

    luhn: 4th iteration

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -2,34 +2,30 @@
 package luhn
 
 import (
-	//"fmt"
-	//"regexp"
-	//"strconv"
 	"strings"
 )
 
 //Valid returns if a string is a valid Luhn string
 func Valid(s string) bool {
-	//re := regexp.MustCompile(pattern)
-	//fmt.Printf("\n***** string = %s *****", s)
 	s = strings.ReplaceAll(s, " ", "")
 	//step 1: compute sum
 	runes := []rune(s)
 	slen, sum := len(runes), 0
 	double := false
-	//fmt.Printf("\nrunes: %s, len: %d\n", s, slen)
-	for i := 1; i <= slen; i++ {
-		digit := int(runes[slen-i] - '0')
+	if slen%2 == 0 {
+		double = true
+	}
+	for _, rV := range runes {
+		digit := int(rV - '0')
 		if digit < 0 || digit > 9 {
 			return false
 		}
 		if double {
-			digit = (digit * 2)
+			digit *= 2
 		}
 		if digit > 9 {
-			digit = digit - 9
+			digit -= 9
 		}
-		//fmt.Printf(", sum = %d; ", sum)
 		sum += digit
 		double = !double
 	}
