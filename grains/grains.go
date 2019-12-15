d72947972a72148d55fc518ba22b0d510366a6f4
vajjhala <vajjhala@bu.edu>
2019-12-15 14:19:33 -0500

    iteration 2

--- grains/grains.go
+++ grains/grains.go
@@ -5,17 +5,19 @@ import (
 	"errors"
 )
 
+var grains uint64
+
 //Square returns the number of grains on the square
 func Square(n int) (uint64, error) {
 	if !(n > 0 && n <= 64) {
 		return 0, errors.New("number out of chessboard range.")
 	}
 	// start with 1grain
-	grains := 1
+	grains = 1
 	for i := 1; i < n; i++ {
 		grains *= 2
 	}
-	return uint64(grains), nil
+	return grains, nil
 }
 
 //Total returns the maximum number of grains on a chessboard
