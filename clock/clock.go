ab22ffaf3dd9707ff410a124c1cdf567e2cf6c3e
vajjhala <vajjhala@bu.edu>
2020-01-02 15:58:14 -0500

    submission 1

--- clock/clock.go
+++ clock/clock.go
@@ -1,53 +1,66 @@
 //Package clock ..
 package clock
 
-import "strconv"
+import (
+	// "fmt"
+	"strconv"
+)
 
 //Clock ...
 type Clock struct {
-    hour, minute int
+	hour, minute int
+}
+
+//clock ..
+func (c Clock) clock() Clock {
+	x, y, z := c.hour%24, c.minute/60, c.minute%60
+	if c.hour < 0 {
+		x = 24 + x
+	}
+	if c.minute < 0 {
+		if z != 0 {
+			y = y - 1
+			z = 60 + z
+		}
+
+	}
+
+	c.hour = (x + y) % 24
+	if c.hour < 0 {
+		c.hour = 24 + c.hour
+	}
+	c.minute = z
+	return c
 }
 
 //New ...
-func New(hour, minute int) Clock  {
-    var c Clock
-    // initialise and Add
-    hour, minute = hour%24, minute%24
-    if hour < 0 {
-        hour = 24 - hour
-        if minute < 0 {
-            minute = 24 - minute
-        }
-    }
-    c.hour = hour
-    c.minute = minute
-    return c
-    }
+func New(hour, minute int) Clock {
+	c := Clock{hour, minute}
+	return c.clock()
+}
 
 //String ...
-func (c Clock) String() string  {
-    return strconv.Itoa(c.hour/10) + strconv.Itoa(c.hour%10) + // hours
-":" +                                                         // :
-    strconv.Itoa(c.minute/10) + strconv.Itoa(c.minute%10)      // minutes
+func (c Clock) String() string {
+	hours := strconv.Itoa(c.hour)     //hour as a string
+	minutes := strconv.Itoa(c.minute) //minute as a string
+	if len(hours) != 2 {
+		hours = "0" + hours
+	}
+	if len(minutes) != 2 {
+		minutes = "0" + minutes
+
+	}
+	return hours + ":" + minutes
 }
 
 //Add ...
 func (c Clock) Add(minutes int) Clock {
-    minutes = (c.minute + minutes) % 60
-    hour := minutes / 60
-    if minutes < 0 {
-        minutes = 60 - minutes
-        if hour < 0 {
-            hour*= -1
-        }
-    }
-    c.hour = c.hour + hour
-    c.minute = minutes
-    return c
+	c.minute += minutes
+	return c.clock()
 }
 
 //Subtract ...
 func (c Clock) Subtract(minutes int) Clock {
-    minutes*= -1
-    return c.Add(minutes)
+	c.minute -= minutes
+	return c.clock()
 }
